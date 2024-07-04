9c2cf07bf58ab41df8282865a7fcfa4cde44712d
CyBueze <caretsquare@gmail.com>
2024-07-04 15:49:29 +0100

    initial upload

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
-function loadPharmacyData(pharmacyId) {
-  fetch(`json/${pharmacyId}.json`)
-    .then(response => response.json())
-    .then(data => {
-      document.title = data.title;
-      document.getElementById('pharmacyName').textContent = data.name;
-      document.getElementById('pharmacyLogo').src = data.logo;
-      document.getElementById('pharmacyContact').textContent = data.contact;
-      document.getElementById('pharmacyAddress').textContent = data.address;
-      document.getElementById('pharmacyContent').textContent = data.content;
-    })
-    .catch(error => console.error('Error fetching pharmacy data:', error));
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   const urlParams = new URLSearchParams(window.location.search);
-  const pharmacyId = urlParams.get('pharmacy');
+  const pharmacyName = urlParams.get('pharmacy');
   
-  if (pharmacyId) {
-    loadPharmacyData(pharmacyId);
+  if (pharmacyName) {
+    document.getElementById('pharmacyName').textContent = pharmacyName;
   }
 });
